ecac5afe382cb9dc03ed10a2547e5712f2beddb1
LuisMiguelPastranaSaenz <136538379+LuisMiguelPastranaSaenz@users.noreply.github.com>
2024-08-16 07:17:20 -0500

    Add files via upload
    Introduccion sintaxis

---
+++ 001-introduccion.py
@@ -0,0 +1,56 @@
+"""
+Introduccion a la sintaxis en este caso. 
+vamos a ver como se imprime por la terminal en python 
+al igual que vamos a ver los operadores basicos en programación 
+"""
+
+#La funcion Print nos sirve para mostrar un mensaje por consola 
+
+print(2 + 3) # Adición(+) 
+print(3 - 1) # Resta(-)
+print(3*2) # Multiplicación 
+print(3/2) # División (/)
+print(3 % 2) # módulo(%) el modulo devuelve la parte entera de la division de cirtos numeros 
+print(3 ** 2) # Operaciones Exponenciales Ej: 2^2
+print(3 // 2 ) #Division entera sin dejar residuo 
+
+# Comprobacion de tipos de datos 
+#Para comprobar de que tipo es la variable o el dato almacenado usamos la funcion type junto a print para mostrar el tipo de dato en consola 
+
+print(type(10)) # Int 
+print(type(3.14)) # float 
+print(type(3 + 3j)) # Operación compleja
+print(type("Lorem")) # String 
+print(type([1,2,3])) # Lista 
+print(type({"Nombre" : "Loremp"})) # Dicionario 
+print(type({9.8,3.14,2.7})) # colocar 
+print(type(9.8,3.14,2.7)) # Tupla 
+print(type(True)) # Boolean 
+# Definicion de tipos de datos 
+
+"""
+Int: Tipo de dato numerico entero Ej: 12 
+Float: Tipo de dato numerico decimal con , o . Ej: 3,454
+String: Cadena de texto esta se representa en unas comillas dobles o simples en python es lo mismo "" y '' para representar los String 
+Lista: Una forma de almacenar un tipo de dato de manera secuencial y ordenada estos se pueden recorrer y cargar 
+Diccionario: Es una forma de almacenar elementos como se almacenarian en las listas pero la diferencia es que puedes asignarle una clave fija y accedes al elemento utilizando la clave 
+Tupla: un conjunto ordenado e inmutable de elementos del mismo o diferente tipo 
+Boolean: Estos solo son dos True o False Se usan para realizar validaciones por ejemplo ¿La luz esta encendida? True seria Si! y False seria No¡
+"""
+
+#Ejercicios Propuestos 
+
+# 1. Teniendo en cuenta lo visto realiza 5 operaciones usando todos los operadores aritmeticos muestralos por consola luego sube el archivo a el repositorio de git 
+"""
+2. Compruebe los siguentes datos 
+10
+9.8
+3.14
+4-4j
+['Asabeneh', 'Python', 'Finlandia']
+Su nombre
+Tu apellido
+Tu país
+teniendo encuenta la estructura mencionada y usando la funcion Type 
+"""
+
